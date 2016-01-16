d9dd943c12bb64e90f0348f7e4986e433c667f4b
jmramos02 <ramosjosemari@gmail.com>
2016-01-16 20:40:54 +0800

    add nullable to url

--- database/migrations/2015_12_26_102737_create_products_table.php
+++ database/migrations/2015_12_26_102737_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('description');
-            $table->string('url');
+            $table->string('url')->nullable();
             $table->double('price');
             $table->string('category');
             $table->string('dosage');
