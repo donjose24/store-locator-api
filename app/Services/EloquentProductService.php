6bbdd708822bfe3605ee5643efc3220dfaa11ceb
Jose Mari Ramos <ramosjosemari@gmail.com>
2016-10-15 21:44:58 +0800

    Whut

--- app/Services/EloquentProductService.php
+++ app/Services/EloquentProductService.php
@@ -5,11 +5,12 @@ namespace App\Services;
 use App\Product;
 use App\Contracts\ProductService;
 
-class EloquentProductService implements ProductService {
+class EloquentProductService implements ProductService
+{
 
-    public function create($input) 
+    public function create($input)
     {
-        $product = new Product(); 
+        $product = new Product();
         $product->name = $input->name;
         $product->desc = $input->desc;
         $product->url = $input->url;
@@ -18,7 +19,7 @@ class EloquentProductService implements ProductService {
         return $product;
     }
 
-    public function update($id, $params) 
+    public function update($id, $params)
     {
         $product = Product::find($id);
         $product->name = $params->name;
@@ -29,21 +30,22 @@ class EloquentProductService implements ProductService {
         return $product;
     }
 
-    public function delete($id) 
+    public function delete($id)
     {
-       $product = Product::find($id); 
-       $product->delete();
+        $product = Product::find($id);
+        $product->delete();
     }
 
-    public function all() 
+    public function all()
     {
-        return Product::all();
+        return Product::with('stores')->get();
     }
 
-    public function search($query) 
+    public function search($query)
     {
         return Product::where('name', 'LIKE', "%$query%")
             ->orWhere('category', 'LIKE', "%$query%")
+            ->with('stores')
             ->get();
     }
 }
